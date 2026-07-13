c76cf7f9b82e66f28e9c4f3f809ff49ba8710ad1
Vinay <vinaypagidipalli2@gmail.com>
2026-07-13 14:08:14 +0530

    PLSQL

---
+++ PLSQL/FreeSQL-demo.sql
@@ -0,0 +1,28 @@
+DECLARE
+    CURSOR c_customers IS
+        SELECT c.CustomerID, c.DOB, l.LoanID, l.InterestRate
+        FROM Customers c
+        JOIN Loans l
+        ON c.CustomerID = l.CustomerID;
+
+    v_age NUMBER;
+BEGIN
+    FOR rec IN c_customers LOOP
+
+        v_age := FLOOR(MONTHS_BETWEEN(SYSDATE, rec.DOB) / 12);
+
+        IF v_age > 60 THEN
+            UPDATE Loans
+            SET InterestRate = InterestRate - 1
+            WHERE LoanID = rec.LoanID;
+
+            DBMS_OUTPUT.PUT_LINE(
+                'Discount applied to Loan ID: ' || rec.LoanID
+            );
+        END IF;
+
+    END LOOP;
+
+    COMMIT;
+END;
+/
